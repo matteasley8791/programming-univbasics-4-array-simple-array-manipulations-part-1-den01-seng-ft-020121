ca1ea045eec706f13300466bedf0d2e9e1449c9c
Matthew Easley <matteasley8791@gmail.com>
2021-01-14 18:43:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,14 +10,14 @@ def using_pop(array)
 array.pop
 end 
 
-def pop_with_args(small_dogs)
-  small_dogs.pop[2] 
+def pop_with_args(array)
+  array.pop[2] 
 end 
 
 def using_shift(array)
   array.shift 
 end 
 
-def shift_with_args(small_dogs)
-  small_dogs.shift[2]
+def shift_with_args(array)
+  array.shift[2]
 end 
